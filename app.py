76b722244029a34d474f35a224843b25b39352d6
khushi_suryawanshi <khushisuryawanshi006@gmail.com>
2026-03-27 12:39:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://sepsis-delta.vercel.app],  # allow all (easy fix)
+    allow_origins=["http://localhost:5173","https://sepsis-delta.vercel.app"],  # allow all (easy fix)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
